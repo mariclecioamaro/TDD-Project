1f67c5957269a169f4bcb4651a7334088953c280
MARICLECIO AMARO DA COSTA <79290613+mariclecioamaro@users.noreply.github.com>
2024-06-30 12:03:59 -0300

    Add files via upload

---
+++ tests/usecases/test_product.py
@@ -0,0 +1,55 @@
+from typing import List
+from uuid import UUID
+from store.core.exceptions import NotFoundException
+from store.schemas.product import ProductOut, ProductUpdateOut
+from store.usecases.produc import product_usecase
+
+import pytest
+
+
+async def test_usecases_create_should_return_success(product_in):
+    result = await product_usecase.create(body=product_in)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == 'Iphone 14 Pro Max'
+
+
+async def test_usecases_get_should_return_success(product_inserted):
+    result = await product_usecase.get(id=product_inserted.id)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == 'Iphone 14 Pro Max'
+
+
+async def test_usecases_get_should_not_found():
+
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.get(id=UUID('1cf214e-85f7-461a-89je-a751a32e3bb9'))
+    
+    assert err.value.message == "Product not found whith filter: 1cf214e-85f7-461a-89je-a751a32e3bb9"
+
+@pytest.mark.usefixture("products_inserted")
+async def test_usecases_query_should_return_success():
+    result = await product_usecase.query()
+
+    assert isinstance(result, List)
+    assert len(result) > 1
+
+async def test_usecases_update_should_return_success(product_up, product_inserted):
+    product_up.price = "7.500" 
+    result = await product_usecase.update(id=product_inserted.id, body=product_up)
+
+    assert isinstance(result, ProductUpdateOut)
+
+async def test_usecases_delete_should_return_success(product_inserted):
+    result = await product_usecase.delete(id=product_inserted.id)
+
+    assert result is True
+
+
+async def test_usecases_delete_should_not_found():
+
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.delete(id=UUID('1cf214e-85f7-461a-89je-a751a32e3bb9'))
+    
+    assert err.value.message == "Product not found whith filter: 1cf214e-85f7-461a-89je-a751a32e3bb9"
